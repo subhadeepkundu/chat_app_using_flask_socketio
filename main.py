6f0aafa6b239db5e7cab2f472a360992dbd76e62
Subhadeep Kundu <30840221+subhadeepkundu@users.noreply.github.com>
2018-10-31 21:03:02 +0530

    Update main.py
    code made more readable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
+from flask import request
 from flask_socketio import SocketIO, send
 app=Flask(__name__)
 app.config['SECRET_KEY']='difficult to guess'
